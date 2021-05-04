ea67e7ede1784a83e37ad0d92917f5e55d428b56
gigimic <gigimic@gmail.com>
2021-05-04 15:08:24 +0530

    updates

--- car_eda.py
+++ car_eda.py
@@ -43,6 +43,8 @@ for index, col_n in enumerate(df.columns):
 from sklearn.feature_selection import VarianceThreshold
 import seaborn as sns 
 import matplotlib.pyplot as plt
+# %matplotlib inline
+# matplotlib.use('Agg')
 print(sns.__version__)
 sns.pairplot(df)
 plt.show()
@@ -95,7 +97,7 @@ random_grid = {'n_estimators': n_estimators,
                 'min_samples_leaf': min_samples_leaf }
 
 print(random_grid)
-rf = RandomForestRegressor()
-rf_random = RandomizedSearchCV(estimator =rf, param_distributions = random_grid,
-scoring = 'neg_mean_squared_error', n_iter = 10, cv = 5, verbose = 2, random_state = 42, n_jobs =1)
-rf_random.fit(X_train, y_train)
+# rf = RandomForestRegressor()
+# rf_random = RandomizedSearchCV(estimator =rf, param_distributions = random_grid,
+# scoring = 'neg_mean_squared_error', n_iter = 10, cv = 5, verbose = 2, random_state = 42, n_jobs =1)
+# rf_random.fit(X_train, y_train)
